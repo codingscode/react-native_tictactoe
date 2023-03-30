b1aedc40938e2862ba4cba1ae0fce42b59d47948
codingscode <heronnestg@hotmail.com>
2023-03-29 22:19:09 -0300

    parte 1

---
+++ TicTacToe/App.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+
+
+
+function App() {
+  
+   return (
+      <View style={styles.container} >
+         <Text>Tic Tac Toe</Text>
+      </View>
+   )
+}
+
+const styles = StyleSheet.create({
+   container: {
+
+   }
+
+
+})
+
+export default App
