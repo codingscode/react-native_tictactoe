0cdee7f49c77bef342f833540e52c768a7bb147a
codingscode <heronnestg@hotmail.com>
2023-04-02 09:23:47 -0300

    parte 10

--- TicTacToe/App.js
+++ TicTacToe/App.js
@@ -16,17 +16,23 @@ const App = () => {
    ])
 
    const markPosition = (position) => {
-      let temp = [...markers]
-      temp[position] = active_player
+      if (!markers[position]) {
       
-      setMarkers(temp)
+         let temp = [...markers]
+         temp[position] = active_player
+         
+         setMarkers(temp)
+         
+         if (active_player === 'X') { // transfer chances to next player
+            setActive_player('O')
+         }
+         else {
+            setActive_player('X')
+         }
+      }
+
       
-      if (active_player === 'X') { // transfer chances to next player
-         setActive_player('O')
-      }
-      else {
-         setActive_player('X')
-      }
+      
       
       
    }
@@ -43,49 +49,48 @@ const App = () => {
                { markers[0] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Top Mid Cell */}
-            <Pressable style={styles.cell_top_mid} >
+            <Pressable style={styles.cell_top_mid} onPress={() => markPosition(1)} >
                { markers[1] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[1] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Top Right Cell */}
-            <Pressable style={styles.cell_top_right} >
+            <Pressable style={styles.cell_top_right} onPress={() => markPosition(2)} >
                { markers[2] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[2] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Mid Left Cell */}
-            <Pressable style={styles.cell_mid_left} >
+            <Pressable style={styles.cell_mid_left} onPress={() => markPosition(3)} >
                { markers[3] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[3] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Mid Mid Cell */}
-            <Pressable style={styles.cell_mid_mid} >
+            <Pressable style={styles.cell_mid_mid} onPress={() => markPosition(4)} >
                { markers[4] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[4] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Mid Right Cell */}
-            <Pressable style={styles.cell_mid_right} >
+            <Pressable style={styles.cell_mid_right} onPress={() => markPosition(5)} >
                { markers[5] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[5] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Bottom Left Cell */}
-            <Pressable style={styles.cell_bottom_left} >
+            <Pressable style={styles.cell_bottom_left} onPress={() => markPosition(6)} >
                { markers[6] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[6] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Bottom Mid Cell */}
-            <Pressable style={styles.cell_bottom_mid} >
+            <Pressable style={styles.cell_bottom_mid} onPress={() => markPosition(7)} >
                { markers[7] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[7] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
             {/* Bottom Right Cell */}
-            <Pressable style={styles.cell_bottom_right} >
+            <Pressable style={styles.cell_bottom_right} onPress={() => markPosition(8)} >
                { markers[8] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
                { markers[8] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
             </Pressable>
          </View>
          <Pressable style={styles.cancelBTN} >
-            { markers[9] === 'X' && <Image source= {require('./assets/image/cross.png')} style={styles.icon} />}
-            { markers[9] === 'O' && <Image source= {require('./assets/image/zero.png')} style={styles.icon} />}
+            <Image source= {require('./assets/image/replay.png')} style={styles.cancelIcon} />
          </Pressable>
       </SafeAreaView>
    )
